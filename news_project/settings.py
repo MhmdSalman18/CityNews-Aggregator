84b1373b14a957179b1169e1132fd3fa38afaee8
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-08-28 13:14:27 +0530

    Resolved merge conflicts

--- news_project/settings.py
+++ news_project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,11 +133,12 @@ USE_TZ = True
 
 
 # news_project/settings.py
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",  # Ensure this path is correct
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Add this line to specify the directory for collected static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
